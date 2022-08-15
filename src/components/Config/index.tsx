eb054d92aae7059fed3342d6dc4b9b7fb45914d1
pedro moreira <jpedrostonemoreira@gmail.com>
2022-08-15 13:34:45 -0300

    Add prototype of config repo layout

--- src/components/Config/index.tsx
+++ src/components/Config/index.tsx
@@ -1,10 +1,26 @@
 import { useState, useEffect } from 'react';
 import { GoGear } from 'react-icons/go';
 
-export function Config(){
+import { ListRepos } from "../../components/ListRepos";
+import { CardRepos } from "../../components/CardRepos";
+
+type ConfigProps = {
+    layoutRepos: string
+}
+
+export function Config(props: ConfigProps){
+
+    const [configs, setConfigs] = useState<string[]>([
+        'list',
+        'card'
+    ]);
+
     return(
         <div>
             <h2><GoGear /></h2>
+            {
+                props.layoutRepos == configs[0] ? <ListRepos /> : <CardRepos />
+            }
         </div>
     )
 }
